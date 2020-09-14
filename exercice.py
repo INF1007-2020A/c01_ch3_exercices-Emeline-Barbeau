c9f6c86a7a85e213860a730269cef63cd8cb8400
Emeline-Barbeau <emeline.barbeau@polymtl.ca>
2020-09-14 18:24:14 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -25,7 +25,7 @@ def to_celsius(temperature: float) -> float:
 
 
 def to_farenheit(temperature: float) -> float:
-    return (temperature × 9/5) + 32 = 32 °F
+    return (temperature × 9/5) + 32
 
 
 def main() -> None:
