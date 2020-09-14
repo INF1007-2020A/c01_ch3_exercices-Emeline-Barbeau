7051c324577b791460f31cffe52e9317119a07ea
Emeline-Barbeau <emeline.barbeau@polymtl.ca>
2020-09-14 18:24:59 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -14,7 +14,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    deg = 
+    deg = math.degrees
     minutes = 
     seconds = 
     return 0.0, 0.0, 0.0
